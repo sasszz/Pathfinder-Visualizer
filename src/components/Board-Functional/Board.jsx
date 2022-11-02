2e49bc07f5dc600c8e63ba21741a6bc0f27cb771
sasszz <luchevreuil@gmail.com>
2022-11-02 12:35:05 -0700

    Functional and Class Board Component

--- src/components/Board-Functional/Board.jsx
+++ src/components/Board-Functional/Board.jsx
@@ -4,6 +4,30 @@ import Header from '../Header/Header';
 
 const Board = () => {
   const [grid, setGrid] = useState([]);
+  const [startRow, setStartRow] = useState(10);
+  const [startCol, setStartCol] = useState(10);
+  // const startRowRef = useRef(startRow);
+
+  const getInitialGrid = () => {
+    const grid1 = [];
+    for (let row = 0; row < 30; row++) {
+      const currentRow = [];
+      for (let col = 0; col < 50; col++) {
+        currentRow.push(createNode(col, row));
+      }
+      grid1.push(currentRow);
+    }
+    return grid1;
+  }
+  
+  const createNode = (col, row) => {
+    return {
+      col,
+      row,
+      isStart: row === startRow && col === startCol,
+      previousNode: null,
+    };
+  };
 
   useEffect(() => {
     setGrid(getInitialGrid());
@@ -37,26 +61,27 @@ const Board = () => {
 }
 
 
-const getInitialGrid = () => {
-  const grid1 = [];
-  for (let row = 0; row < 30; row++) {
-    const currentRow = [];
-    for (let col = 0; col < 50; col++) {
-      currentRow.push(createNode(col, row));
-    }
-    grid1.push(currentRow);
-  }
-  console.log(grid1)
-  return grid1;
-}
+// const getInitialGrid = () => {
+//   const grid1 = [];
+//   for (let row = 0; row < 30; row++) {
+//     const currentRow = [];
+//     for (let col = 0; col < 50; col++) {
+//       currentRow.push(createNode(col, row));
+//     }
+//     grid1.push(currentRow);
+//   }
+//   console.log(grid1)
+//   return grid1;
+// }
 
-const createNode = (col, row) => {
-  return {
-    col,
-    row,
-    previousNode: null,
-  };
-};
+// const createNode = (col, row) => {
+//   return {
+//     col,
+//     row,
+//     isStart: row === this.startRow && col === this.startCol,
+//     previousNode: null,
+//   };
+// };
 
 
 
